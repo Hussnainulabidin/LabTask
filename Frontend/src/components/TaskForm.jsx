ef65d8c5e55e5bca16afb41b0e2e58696f8574cd
Hussnain Ul Abidin <113284383+Hussnainulabidin@users.noreply.github.com>
2025-04-21 14:04:16 +0500

    Update TaskForm.jsx

--- Frontend/src/components/TaskForm.jsx
+++ Frontend/src/components/TaskForm.jsx
@@ -18,7 +18,7 @@ function TaskForm({ onTaskAdded }) {
     setError(null);
     
     try {
-      const response = await fetch('http://localhost:8000/api/data', {
+      const response = await fetch('https:/lab-task-api.vercel.app/api/data', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
