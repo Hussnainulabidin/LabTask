84b824f934ebbf4898f942d02915ecab0b705a3c
Hussnain Ul Abidin <113284383+Hussnainulabidin@users.noreply.github.com>
2025-05-05 02:48:48 +0500

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -38,7 +38,7 @@ const Task = mongoose.model('Task', taskSchema);
 
 // Enable CORS for the frontend
 app.use(cors({
-  origin: 'https://lab-task-miox.vercel.app/' // Vite default port
+  origin: '*' // Vite default port
 }));
 
 // Parse JSON request body
